b14a679d1390b9b67988e15c5d9f99c8bd94c521
Khmarigou <luca.plc@outlook.fr>
2024-09-20 20:42:12 +0200

    Add readme and nodejs server launching

---
+++ app.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+
+    let extname = path.extname(filePath);
+
+    let contentType = 'text/html';
+
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+    }
+
+    // Lire le fichier demandé
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('<h1>404 Page not found</h1>', 'utf-8');
+            } else {
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on : http://localhost:${PORT}`);
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Infinite Canvas
+
+Infinite canvas is an infinite canvas _(thanks you captain obvious)_ where you can only draw lines that link edges of the grid.
+
+
+## Runing 
+
+You can use the app by opening the `index.html` in your browser, or run a node.js serveur with :
+```node app.js```
+and access at `http://localhost:3000`.
+
